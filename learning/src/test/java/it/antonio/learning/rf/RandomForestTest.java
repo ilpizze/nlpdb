0337a6fc49e38479748e75b9ce5eea0ea256f96a
Antonio <Antonio@Pc-Casa.lan>
2018-05-28 23:29:52 +0200

    random forest

---
+++ learning/src/test/java/it/antonio/learning/rf/RandomForestTest.java
@@ -0,0 +1,14 @@
+package it.antonio.learning.rf;
+
+import org.junit.Test;
+
+import it.antonio.learning.data.DataSet;
+import it.antonio.learning.data.SimpleDataSet;
+
+public class RandomForestTest {
+
+	@Test
+	public void test() {
+		
+	}
+}
